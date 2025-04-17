71ed738fd23101f54c9fc612afb98ffcafadb1c7
RM <fatima.kasmalykyzy@alatoo.edu.kg>
2025-04-17 10:20:45 +0600

    3

--- .idea/Main.java
+++ .idea/Main.java
@@ -31,6 +31,29 @@ public class Main {
         int[] start = {0, 1};
         int[] end = {rows - 1, cols - 2};
 
+        visited = new boolean[rows][cols];
+        if (dfs(start[0], start[1], end)) {
+            markPath();
+            System.out.println("Path found:");
+        } else {
+            System.out.println("No path found.");
+        }
 
+        printMaze();
     }
+
+    private static void generateMaze() {
+        maze = new char[rows][cols];
+
+        for (int r = 0; r < rows; r++)
+            Arrays.fill(maze[r], WALL);
+
+        maze[1][1] = PATH;
+        carve(1, 1);
+
+        maze[0][1] = PATH;
+        maze[rows - 1][cols - 2] = PATH;
+    }
+
+
 }
