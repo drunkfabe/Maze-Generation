a18c6b3a46c3cc5096f366f6186cecf25e3f6d11
RM <fatima.kasmalykyzy@alatoo.edu.kg>
2025-04-17 09:19:32 +0600

    00

---
+++ .idea/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class MazeSolver {
+
+    private static final char WALL = '#';
+    private static final char PATH = ' ';
+    private static final char VISITED = '.';
+}
